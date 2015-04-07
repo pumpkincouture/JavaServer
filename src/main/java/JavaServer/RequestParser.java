67c8e84da8638ece1c99d5479130fa8f2289d281
Sylwia Olak <pumpkincouture@gmail.com>
2015-04-07 16:46:48 -0500

    Added methods around parsing first line of a response - splitting string into method, path, and HTTP version; updated tests to validate parsing methods

--- src/main/java/JavaServer/RequestParser.java
+++ src/main/java/JavaServer/RequestParser.java
@@ -2,8 +2,22 @@ package JavaServer;
 
 public class RequestParser {
     String request;
+    String[] splitRequest;
 
-    public RequestParser(String HTTPRequest) {
-        this.request = HTTPRequest;
+    public RequestParser(String request) {
+        this.request = request;
+        splitRequest = request.split(" ", 3);
+    }
+
+    public String getMethod() {
+        return splitRequest[0];
+    }
+
+    public String getPath() {
+        return splitRequest[1];
+    }
+
+    public String getHTTPVersion() {
+        return splitRequest[2];
     }
 }
