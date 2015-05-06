360f852e0ff28e95c0458311df529e00f340b31e
Sylwia Olak <pumpkincouture@gmail.com>
2015-05-06 16:41:36 -0500

    GetResponseTest 100% covered.

--- src/test/java/JavaServer/responses/methods/GetResponseTest.java
+++ src/test/java/JavaServer/responses/methods/GetResponseTest.java
@@ -5,10 +5,7 @@ import JavaServer.requests.RequestParser;
 import JavaServer.responses.FileWriter;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,27 +23,46 @@ public class GetResponseTest {
         return out;
     }
 
-    @Test
-    public void returns200ResponseIfRequestValid() throws UnsupportedEncodingException {
-        requestParser = new RequestParser("GET / HTTP/1.1");
+    private void createRequestAndFilePath(String requestLine, String filepath) throws UnsupportedEncodingException {
+        requestParser = new RequestParser(requestLine);
         request = new Request(requestParser.getMethod(), requestParser.getPath(), requestParser.getHeaders(), requestParser.getData());
-        path = new File("/Users/test/code/JavaServer/public/image.gif");
+        path = new File("/Users/test/code/JavaServer/public/" + filepath);
         fileWriter = new FileWriter(path, mockDataStream());
-
         response = new GetResponse(fileWriter, request);
+    }
+
+    @Test
+    public void returns200ResponseCode() throws UnsupportedEncodingException {
+        createRequestAndFilePath("GET / HTTP/1.1", "");
 
         assertEquals("HTTP/1.1 200 OK", response.getCorrectStatus());
     }
 
     @Test
-    public void returnsBlankHeaderIfPathIsNotForMethodOptions() throws UnsupportedEncodingException {
-        requestParser = new RequestParser("GET / HTTP/1.1");
-        request = new Request(requestParser.getMethod(), requestParser.getPath(), requestParser.getHeaders(), requestParser.getData());
-        path = new File("/Users/test/code/JavaServer/public/image.gif");
-        fileWriter = new FileWriter(path, mockDataStream());
-
-        response = new GetResponse(fileWriter, request);
+    public void returnsEmptyStringAsHeader() throws UnsupportedEncodingException {
+        createRequestAndFilePath("GET / HTTP/1.1", "");
 
         assertEquals("", response.getCorrectHeaders());
+    }
+
+    @Test
+    public void returnsDirecoryLinksIfFileExistsAndIsRoute() throws IOException {
+        createRequestAndFilePath("GET / HTTP/1.1", "");
+
+        assertEquals("<a href='/file1'>file1</a></br>\r\n" +
+                     "<a href='/file2'>file2</a></br>\r\n" +
+                     "<a href='/image.gif'>image.gif</a></br>\r\n" +
+                     "<a href='/image.jpeg'>image.jpeg</a></br>\r\n" +
+                     "<a href='/image.png'>image.png</a></br>\r\n" +
+                     "<a href='/partial_content.txt'>partial_content.txt</a></br>\r\n" +
+                     "<a href='/patch-content.txt'>patch-content.txt</a></br>\r\n" +
+                     "<a href='/text-file.txt'>text-file.txt</a></br>\r\n", response.getCorrectBody());
+    }
+
+    @Test
+    public void callsFileManagerToSendOutFileContentsAndReturnsAnEmptyString() throws IOException {
+        createRequestAndFilePath("GET /form HTTP/1.1", "form");
+
+        assertEquals("", response.getCorrectBody());
     }
 }
