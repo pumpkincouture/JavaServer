ecbc6388d84a5da3cb3b1555938d3a9fa2506941
Sylwia Olak <pumpkincouture@gmail.com>
2015-05-06 18:16:30 -0500

    Redirect Response 100% covered.

--- src/test/java/JavaServer/responses/methods/RedirectResponseTest.java
+++ src/test/java/JavaServer/responses/methods/RedirectResponseTest.java
@@ -5,6 +5,7 @@ import JavaServer.requests.RequestParser;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,13 +14,30 @@ public class RedirectResponseTest {
     private RequestParser requestParser;
     private Response response;
 
+    private void createRequestAndFilePath(String requestLine) throws UnsupportedEncodingException {
+        requestParser = new RequestParser(requestLine);
+        request = new Request(requestParser.getMethod(), requestParser.getPath(), requestParser.getHeaders(), requestParser.getData());
+        response = new RedirectResponse();
+    }
+
     @Test
     public void returns302IfPathIsRedirect() throws IOException {
-        requestParser = new RequestParser("GET /redirect HTTP/1.1");
-        request = new Request(requestParser.getMethod(), requestParser.getPath(), requestParser.getHeaders(), requestParser.getData());
-
-        response = new RedirectResponse();
+        createRequestAndFilePath("GET /redirect HTTP/1.1");
 
         assertEquals("HTTP/1.1 302 Found", response.getCorrectStatus());
+    }
+
+    @Test
+    public void returnLocationHeaderAsHeader() throws UnsupportedEncodingException {
+        createRequestAndFilePath("GET /redirect HTTP/1.1");
+
+        assertEquals("Location: http://localhost:5000/", response.getCorrectHeaders());
+    }
+
+    @Test
+    public void returnEmptyStringAsBody() throws IOException {
+        createRequestAndFilePath("GET /redirect HTTP/1.1");
+
+        assertEquals("", response.getCorrectBody());
     }
 }
