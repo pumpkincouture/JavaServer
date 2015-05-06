7a50b2d82f88f89b01582af29b1ba47df12474be
Sylwia Olak <pumpkincouture@gmail.com>
2015-05-06 17:06:41 -0500

    OptionsResponse 100% covered.

--- src/test/java/JavaServer/responses/methods/OptionsResponseTest.java
+++ src/test/java/JavaServer/responses/methods/OptionsResponseTest.java
@@ -4,6 +4,8 @@ import JavaServer.requests.Request;
 import JavaServer.requests.RequestParser;
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.junit.Assert.assertEquals;
 
 public class OptionsResponseTest {
@@ -11,12 +13,30 @@ public class OptionsResponseTest {
     private RequestParser requestParser;
     private Response response;
 
-    @Test
-    public void returns200ResponseIfRequestPathValid() {
-        requestParser = new RequestParser("OPTIONS /method_options HTTP/1.1");
+    private void createRequestAndResponse(String requestLine) {
+        requestParser = new RequestParser(requestLine);
         request = new Request(requestParser.getMethod(), requestParser.getPath(), requestParser.getHeaders(), requestParser.getData());
         response = new OptionsResponse();
+    }
+
+    @Test
+    public void returns200ResponseStatus() {
+        createRequestAndResponse("OPTIONS /method_options HTTP/1.1");
 
         assertEquals("HTTP/1.1 200 OK", response.getCorrectStatus());
+    }
+
+    @Test
+    public void returnsOptionsHeader() {
+        createRequestAndResponse("OPTIONS /method_options HTTP/1.1");
+
+        assertEquals("Allow: GET,HEAD,POST,OPTIONS,PUT", response.getCorrectHeaders());
+    }
+
+    @Test
+    public void returnsEmptyStringAsBody() throws IOException {
+        createRequestAndResponse("OPTIONS /method_options HTTP/1.1");
+
+        assertEquals("", response.getCorrectBody());
     }
 }
