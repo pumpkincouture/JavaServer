06c904c2b65206f4c8e16263928936d887f8bded
Sylwia Olak <pumpkincouture@gmail.com>
2015-05-06 17:00:51 -0500

    LogsResponseTest now 100% covered.

---
+++ src/test/java/JavaServer/responses/methods/LogsResponseTest.java
@@ -0,0 +1,55 @@
+package JavaServer.responses.methods;
+
+import JavaServer.requests.Logger;
+import JavaServer.requests.Request;
+import JavaServer.requests.RequestParser;
+import org.junit.Test;
+
+import java.io.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class LogsResponseTest {
+    private Response response;
+    private Logger logger;
+    private RequestParser requestParser;
+    private Request request;
+
+    private void createRequestAndResponse(String requestLine) {
+        requestParser = new RequestParser(requestLine);
+        request = new Request(requestParser.getMethod(), requestParser.getPath(), requestParser.getHeaders(), requestParser.getData());
+        logger = new Logger();
+        logger.logRequest(request.getRequestLine());
+        response = new LogsResponse(logger);
+    }
+
+    @Test
+    public void returns200AsStatus() {
+        createRequestAndResponse("GET /logs HTTP/1.1\n" +
+                                 "Authorization: Basic YWRtaW46aHVudGVyMg==\n" +
+                                 "Host: localhost:5000\n" +
+                                 "\n");
+
+        assertEquals("HTTP/1.1 200 OK", response.getCorrectStatus());
+    }
+
+    @Test
+    public void returnsEmptyStringAsHeader() {
+        createRequestAndResponse("GET /logs HTTP/1.1\n" +
+                                 "Authorization: Basic YWRtaW46aHVudGVyMg==\n" +
+                                 "Host: localhost:5000\n" +
+                                 "\n");
+
+        assertEquals("", response.getCorrectHeaders());
+    }
+
+    @Test
+    public void returnsLoggingDataAsBody() throws IOException {
+        createRequestAndResponse("GET /logs HTTP/1.1\n" +
+                                 "Authorization: Basic YWRtaW46aHVudGVyMg==\n" +
+                                 "Host: localhost:5000\n" +
+                                 "\n");
+
+        assertEquals("GET /logs HTTP/1.1\r\n", response.getCorrectBody());
+    }
+}
